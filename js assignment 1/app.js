a5c2f950ec9bb0af49c7e0b592deee29e29c88bc
mariamsabir <mariamsabir4181@gmail.com>
2025-09-04 15:10:10 +0500

    js assignments

---
+++ js assignment 1/app.js
@@ -0,0 +1,318 @@
+//ALERT
+// 1)
+    // alert("Hello! Welcome to my website 😊");
+
+// 2)
+// alert("Error! please enter a valid passward");
+
+// 3)
+// alert("Welcome To JS Land... \n Happy Coding!");
+
+// 4)
+// alert("Welcome To JS Land...");
+// alert("Happy Coding!");
+
+// 5)
+// alert("Hello... I can run JS through my web browser's console");
+// console.log("Hello... I can run JS through my web browser's console");
+
+// 6 and 7 done
+
+// VARIABLES FOR STRING
+// 1)
+// var username = "mariam";
+
+// 2)
+// var myName = "mariamsabir";
+
+// 3)
+// var message;
+// message = "Hello World"
+// alert("message");
+
+// 4)
+// var userName = "John Doe";
+// alert(userName);
+// var birthdate = "15 years old";
+// alert(birthdate);
+// var userSkills = "Certified Mobile Application Development";
+// alert(userSkills);
+
+// 5)
+// alert("PIZZA \n PIZZ \n PIZ \n PI \n P");
+
+// 6_)
+// var Email = "example@example.com";
+// alert("My email address is" + " " + Email);
+
+// 7)
+// var Book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the book" + " " + Book)
+
+// 8)
+// document.write("Yah! I can write HTML content through JavaScript")
+
+// 9)
+// var char = "---------&@!@&---------";
+// alert(char);
+
+// VARIABLES FOR NUMBERS
+// 1)
+// var age = "I am 15 years old"
+// alert(age)
+
+// 2)
+// alert("you have visited this site N times")
+
+
+// 3)
+// var birthYear = "birth year";
+// document.write ("My"+ " " + birthYear + " " + "is 1990");
+// document.write("Datatype of my ddeclared variable is number");
+
+// 4)
+// var store = "xyzClothing.com";
+// document.write("John Doe ordered 5T-Shirt(s) on" + " " + store);
+
+// VARIABLE NAMES: LEGAL & ILLEGAL
+// 1)
+// var a = "1" , b = "2" , z = "3"
+
+// 2)
+
+// legal variables
+// var name$ = "";
+// var $count = "";
+// var myName = "";
+// var _age = "";
+// var Number = "";
+
+// illegal variables
+// var %NAME = "";
+// var *age = "";
+// var @count = "";
+// var !char = "";
+// var ^myName = "";
+
+
+// 3)
+// document.write("Variable names can only contain . numbers.$ and_. For example : $My_1st variable" + " " );
+// document.write("variable must begin with a letter , $ or_. for example : $name ,_ name or name");
+// document.write("variable names are case sensitive ");
+// document.write("variable names should not be JS keyword")
+
+// MATH EXPRESSIONS 
+// 1)
+// var a = 3;
+// var b = 5;
+// var c = a+b
+// console.log("sum of 3 and 5 is " + c);
+
+
+// 2)
+// var a = 3;
+// var b = 5;
+// var c = a-b
+// console.log("sum of 3 and 5 is " + c);
+// var d = 3;
+// var e = 5;
+// var f = a*b
+// console.log("sum of 3 and 5 is " + f);
+// var g = 3;
+// var h = 5;
+// var i = a/b
+// console.log("sum of 3 and 5 is " + i);
+// var j = 3;
+// var k = 5;
+// var l = a%b
+// console.log("sum of 3 and 5 is " + l);
+
+
+// 3
+// var char = "";
+// var a = 5;
+// console.log("initial value : " + a);
+// var b = ++a;
+// console.log("value after increment is : " + b);
+// var c = 7+ b;
+// console.log("value after addition is : " + c);
+// var d = c-1 ;
+// console.log("value after decrement is : " +  d);
+// var e = "0";
+// console.log("the reminder is 0");
+
+
+// 4)
+// var ticket = "600";
+// var buy = "5";
+// var calc = "ticket % buy"
+// document.write("total cost to buy 5 tickets to a movie is 300PKR");
+
+// 5)
+// var a ="table of 4"
+// console.log(a);
+// var b =4
+// console.log(b*1);
+// var c =4
+// console.log(c*2);
+// var d =4
+// console.log(d*3);
+// var e =4
+// console.log(e*4);
+// var f =4
+// console.log(f*5);
+// var g =4
+// console.log(g*6);
+// var h =4
+// console.log(h*7);
+// var i =4
+// console.log(i*8);
+// var j =4
+// console.log(j*9);
+// var k =4
+// console.log(k*10);
+
+// 6)
+// var a = ("c=(F-32)*5/9");
+// var b = (F=(C*5/9) +32)
+// console.log("25 is 77");
+// console.log("70 is 21.11111111");
+
+// 7) done
+ // Step 1: Store items in variables
+        // let item1 = { name: "Laptop", price: 800, quantity: 1 };
+        // let item2 = { name: "Mouse", price: 20, quantity: 2 };
+        // let item3 = { name: "Keyboard", price: 50, quantity: 1 };
+
+        // // Step 2: Store all items in an array
+        // let cart = [item1, item2, item3];
+
+        // // Step 3: Calculate subtotal
+        // let subtotal = 0;
+        // for (let item of cart) {
+        //     subtotal += item.price * item.quantity;
+        // }
+
+        // // Step 4: Apply tax (e.g., 10%)
+        // let taxRate = 0.10;
+        // let tax = subtotal * taxRate;
+
+        // // Step 5: Calculate total
+        // let total = subtotal + tax;
+
+        // // Step 6: Display checkout summary
+        // console.log("----- Checkout Summary -----");
+        // for (let item of cart) {
+        //     console.log(`${item.name} x ${item.quantity} = $${item.price * item.quantity}`);
+        // }
+        // console.log(`Subtotal: $${subtotal}`);
+        // console.log(`Tax (10%): $${tax}`);
+        // console.log(`Total: $${total}`);
+
+
+
+// MATH EXPRESSIONS CHAP: 6-9
+// 1)
+// console.log("RESULT");
+// var a = 10;
+// console.log("The value of a is: 10");
+
+// var b = ++a;
+// console.log("The value of ++a is :" +b);
+// console.log("now the value of a is:" + " " + b );
+
+// var c = a++;
+// console.log("The value of ++a is :" + c);
+// console.log("now the value of a is:" + " " + ++c );
+
+// var d = --a;
+// console.log("The value of --a is :" + d);
+// console.log("now the value of a is:" + " " + d );
+
+// var e = a--;
+// console.log("The value of --a is :" + e--);
+// console.log("now the value of a is:" + " " + e );
+
+// 2)
+// var a = 2 , b = 1;
+// var result = --a - --b + ++b + b--; 
+// //             1 - 0 + 1 +1
+// var c = a--;
+// console.log(c);
+// var d = --a - --b;
+// console.log(d);
+
+// var e = --a - --b + ++b;
+// console.log(e);
+
+
+// var f = --a - --b + ++b + b--;
+// console.log(f);
+
+
+// 3)
+// let name = prompt("Enter your name:");  
+// alert("Hello, " + name + "! Welcome!");
+
+// 4)donee
+
+// 5)
+
+    // let num = prompt("Enter a number to see its multiplication table:", "5");
+
+    // num = num ? parseInt(num) : 5;
+
+    // document.write("<h3>Multiplication Table of " + num + "</h3>");
+    // for (let i = 1; i <= 10; i++) {
+    //   document.write(num + " x " + i + " = " + (num * i) + "<br>");
+    // }
+
+
+    // 6)
+    //     // Step a: Take 3 subject names
+
+    // let sub1 = prompt("Enter name of first subject:");
+    // let sub2 = prompt("Enter name of second subject:");
+    // let sub3 = prompt("Enter name of third subject:");
+
+    // // Step b: Total marks for each subject
+    // let totalMarks = 100;
+
+    // // Step c & d: Obtained marks
+
+    // let marks1 = parseInt(prompt("Enter obtained marks for " + sub1 + ":"));
+    // let marks2 = parseInt(prompt("Enter obtained marks for " + sub2 + ":"));
+    // let marks3 = parseInt(prompt("Enter obtained marks for " + sub3 + ":"));
+
+    // // Step e: Calculate total and percentage
+
+    // let obtainedTotal = marks1 + marks2 + marks3;
+    // let grandTotal = totalMarks * 3;
+    // let percentage = (obtainedTotal / grandTotal) * 100;
+
+    // // Display result in table
+
+    // document.write("<table border='1' cellspacing='0' cellpadding='8'>");
+    // document.write("<tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th></tr>");
+    // document.write("<tr><td>" + sub1 + "</td><td>" + totalMarks + "</td><td>" + marks1 + "</td></tr>");
+    // document.write("<tr><td>" + sub2 + "</td><td>" + totalMarks + "</td><td>" + marks2 + "</td></tr>");
+    // document.write("<tr><td>" + sub3 + "</td><td>" + totalMarks + "</td><td>" + marks3 + "</td></tr>");
+    // document.write("<tr><th>Total</th><th>" + grandTotal + "</th><th>" + obtainedTotal + "</th></tr>");
+    // document.write("</table>");
+    // document.write("<h3>Percentage: " + percentage.toFixed(2) + "%</h3>");
+
+
+// 7)  done
+
+
+
+
+
+
+
+
+
+
+
+
+
